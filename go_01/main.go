a80fbd3a983e41db8015962a4f823dd4b7611f5c
obkin <kapellwork@gmail.com>
2024-01-08 23:08:39 +0200

    learned: aboutStr / aboutStructures / aboutArr / aboutBooleanAndIfElse

--- go_01/main.go
+++ go_01/main.go
@@ -5,17 +5,55 @@ import "fmt"
 type Human struct {
 	Name     string
 	age      int
-	email    string
-	password string
+	Email    string
+	Password string
 }
 
+// entry point
 func main() {
+	// aboutStr()
+	// aboutStructures()
+	// aboutArr()
+	aboutBooleanAndIfElse()
+}
+
+// practise
+
+func aboutStr() {
+	hello := "Hello"
+	world := "World!"
+
+	helloWorld := hello + " " + world
+	fmt.Println(helloWorld)
+}
+
+func aboutStructures() {
 	person := Human{
 		Name:     "Yarok",
 		age:      21,
-		email:    "yarok@gmail.com",
-		password: "qwerty1234567890",
+		Email:    "kapellwork@gmail.com",
+		Password: "qwerty123",
 	}
 
 	fmt.Println(person)
+}
+
+func aboutArr() {
+	var arr [5]int
+	arr[1] = 2
+
+	fmt.Println(arr)
+}
+
+func aboutBooleanAndIfElse() {
+	var alive bool
+
+	alive = false
+	alive = true
+
+	if alive {
+		fmt.Println("I'm alive!")
+	} else {
+		fmt.Println("I'm dead.")
+	}
 }
