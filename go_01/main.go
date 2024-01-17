a117b082749a1ed7dc34dca072eeb58da4585ead
obkin <kapellwork@gmail.com>
2024-01-17 23:08:44 +0200

    learned: aboutDigits, aboutSlices

--- go_01/main.go
+++ go_01/main.go
@@ -14,7 +14,9 @@ func main() {
 	// aboutStr()
 	// aboutStructures()
 	// aboutArr()
-	aboutBooleanAndIfElse()
+	// aboutBooleanAndIfElse()
+	// aboutDigits()
+	aboutSlices()
 }
 
 // practise
@@ -40,9 +42,18 @@ func aboutStructures() {
 
 func aboutArr() {
 	var arr [5]int
-	arr[1] = 2
+	fmt.Println(arr) // [0,0,0,0,0]
 
-	fmt.Println(arr)
+	arr[1] = 2
+	fmt.Println(arr) // [0,2,0,0,0]
+
+	arr[1] = 10
+	fmt.Println(arr) // [0,10,0,0,0]
+
+	arrWithValues := [3]int{34, 12, 4}
+	fmt.Println(arrWithValues) // [34, 12, 4]
+
+	fmt.Println(arrWithValues[2]) // 4
 }
 
 func aboutBooleanAndIfElse() {
@@ -56,4 +67,29 @@ func aboutBooleanAndIfElse() {
 	} else {
 		fmt.Println("I'm dead.")
 	}
+}
+
+func aboutDigits() {
+	var num1 int64 = 1234432
+	var num2 float32 = 1.24
+
+	var num3 uint16 = 451
+	var num4 int16 = 12343
+
+	fmt.Println(num1, num2, num3, num4)
+	fmt.Println(int16(num3) + num4)
+}
+
+func aboutSlices() {
+	week := [...]int{0, 1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(week)      // [0, 1, 2, 3, 4, 5, 6, 7]
+	fmt.Println(week[2:6]) // [2, 3, 4, 5]
+
+	weekend := week[6:8]
+	fmt.Println(weekend)      // [6, 7] (days, end of the week)
+	fmt.Println(week[6:])     // [6, 7] (days, end of the week)
+	fmt.Println(len(weekend)) // 2
+
+	some := [...]int{13, 11, 24, 35, 47, 53, 64, 71}
+	fmt.Println(some[0:5]) // [13, 11, 24, 35, 47]
 }
